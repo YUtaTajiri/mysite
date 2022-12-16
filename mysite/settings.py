d77a144eaad0ae29d4ca270640ee4b7402f1721f
Yuta <yuu99626@icloud.com>
2022-12-16 23:01:43 +0900

    'SECRET_KEY変更'

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+try: 
+    from .local_settings import *
+except ImportError:
+    pass    
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-72h_04)grg+4!&2+7^0u_k3kmbetfi_7k#x0v(#z#l#(tz2ia^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
