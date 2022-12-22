47fc3ea9c9a2fc7edcd5b00b977ddb7540c8da8c
Yuta <yuu99626@icloud.com>
2022-12-22 22:44:08 +0900

    'リスト更新日時の取得'

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+import os
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 try: 
     from .local_settings import SECRET_KEY
 except ImportError:
     pass 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
