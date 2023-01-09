a64509d68cb5d1d3696c76ae5060d551bec51d5a
Yuta <yuu99626@icloud.com>
2023-01-09 16:34:12 +0900

    'リストを自動で更新するように修正'

--- myapp/views.py
+++ myapp/views.py
@@ -1,35 +1,29 @@
 from django.shortcuts import render
 from myapp.application import django_test1
-from random import randint
 from django.views.generic import CreateView
 import datetime
+import schedule
+
+
 
 # Create your views here.
 
 def create_list(request):
     template_name =  'app_folder_html/index.html'
     if not django_test1.list1:
-        if request.method == 'POST' :
-            if 'get_button' in request.POST:
-                django_test1.get_title1()
+        django_test1.get_title1()
     if not django_test1.list2:
-        if request.method == 'POST' :
-            if 'get_button' in request.POST:
-                django_test1.get_title2()
+        django_test1.get_title2()
     if not django_test1.list3:
-        if request.method == 'POST' :
-            if 'get_button' in request.POST:
-                django_test1.get_title3()
+        django_test1.get_title3()
     list_get1 = django_test1.list1        
     list_get2 = django_test1.list2
     list_get3 = django_test1.list3
-    random_int = randint(1, 10)
     dt = datetime.datetime.now().replace(microsecond=0)
     context ={
         'dt_now' : str(dt),
         'get': list_get1,
         'get2': list_get2,
-        'get3': list_get3,
-        'random_number' : str(random_int), 
+        'get3': list_get3, 
     }
     return render(request, template_name, context)
